e3f32df5707f265d18667e0ced7454c5b6b181e7
John Kyle Salaysay <foursalaysay@gmail.com>
2024-07-21 22:25:06 +0800

    for further study in routing / navigation

--- app/utils/AppNavigator.ts
+++ app/utils/AppNavigator.ts
@@ -1,1 +1,15 @@
-import React from "r
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>{/* ... */}</Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigator;

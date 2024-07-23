70181e01ad1c2aece6fc0fcf937afdeecd60c900
John Kyle Salaysay <foursalaysay@gmail.com>
2024-07-23 21:13:20 +0800

    fixing navigation

---
+++ app/App.tsx
@@ -0,0 +1,13 @@
+import Router
+
+const App = () => {
+    return(
+        <Router>
+        <Stack key="root">
+          <Scene key="login" component={Login} title="Login" />
+          <Scene key="register" component={Register} title="Register" />
+          <Scene key="home" component={Home} />
+        </Stack>
+      </Router>
+    )
+}
